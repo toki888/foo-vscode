a508d47c82d8e110eb8c26919a813bb9e57107e6
funadotown <funadohome@gmail.com>
2023-12-23 11:52:10 +0900

    add more files

---
+++ temp.sql
@@ -0,0 +1,5 @@
+CREATE TABLE table_name (
+    column1 datatype1 constraint1,
+    column2 datatype2 constraint2,
+    ...
+);
---
+++ try001.py
@@ -0,0 +1,8 @@
+import os, sys
+import pandas as pd
+def sum(a, b):
+    c = a + b
+    return c
+
+
+print("Hello, world!", sum(1,2))

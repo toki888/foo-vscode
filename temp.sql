ebda0ee8e4e15a4c5f89ffbd064ea7eea811102f
funadotown <funadohome@gmail.com>
2023-12-24 12:44:20 +0900

    Added 5 columns in temp.sql file.

--- temp.sql
+++ temp.sql
@@ -1,5 +1,10 @@
 CREATE TABLE table_name (
     column1 datatype1 constraint1,
     column2 datatype2 constraint2,
+    column3 datatype3 constraint3,
+    column4 datatype4 constraint4,
+    column5 datatype5 constraint5,
+    column6 datatype6 constraint6,
+    column7 datatype7 constraint7,
     ...
 );
